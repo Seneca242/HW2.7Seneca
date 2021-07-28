6ce86b3a6a6da06bebc57e65f94effdf69582b26
Seneca <flabbergasted242@gmail.com>
2021-07-28 17:42:04 +0300

    Initial Commit

---
+++ HW2.7Seneca/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.7Seneca
+//
+//  Created by Дмитрий Дмитрий on 28.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
